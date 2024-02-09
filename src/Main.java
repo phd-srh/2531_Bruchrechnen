e56266371155911e8069201bf2d2965f76508d6a
Peter Dillinger <peter.dillinger@srh.de>
2024-02-09 11:05:23 +0100

    Main angepasst, um die Aufgabe zu zeigen

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         Bruch b1 = new Bruch(1, 2);
+        Bruch b2 = new Bruch(2, 4);
+        Bruch b3 = b1.add(b2);
 
-        System.out.println( b1 );
+        System.out.println( b1 + " + " + b2 + " = " + b3 );
     }
 
 }
