49b17073abace8c64bb0441dccb81d6d8709a1f6
Peter Dillinger <peter.dillinger@srh.de>
2024-02-09 11:30:43 +0100

    Addieren klappt jetzt, aber kürzen müsste noch ergänzt werden.

--- src/Bruch.java
+++ src/Bruch.java
@@ -7,6 +7,19 @@ public class Bruch {
         this.nenner = nenner;
     }
 
+    public Bruch add(Bruch b) {
+        long neuerZähler = this.zähler * b.nenner + b.zähler * this.nenner;
+        long neuerNenner = this.nenner * b.nenner;
+        return new Bruch(neuerZähler, neuerNenner).kürze();
+    }
+
+    public Bruch kürze() {
+        long neuerZähler = this.zähler;
+        long neuerNenner = this.nenner;
+        // TODO
+        return new Bruch(neuerZähler, neuerNenner);
+    }
+
     public String toString() {
         return zähler + "/" + nenner;
     }
